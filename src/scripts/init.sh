dbc653558cd52fdd2306b947c301312b3ae25078
yuzh <yuzhuohao2023@iscas.ac.cn>
2025-03-27 09:28:48 +0800

    style: 为train.py加注释

---
+++ src/scripts/init.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install -y clang btop nvtop nano swig python3-dev
--- src/train.py
+++ src/train.py
@@ -5,11 +5,18 @@ import hydra
 from omegaconf import DictConfig
 import omegaconf
 import rich
+from harl.runners import RUNNER_REGISTRY
+
+def _to_dict(cfg1: DictConfig):
+    return omegaconf.OmegaConf.to_container(
+        cfg1, resolve=True, throw_on_missing=True
+    )
 
 @hydra.main(config_path="configs", config_name="config", version_base=None)
 def main(cfg: DictConfig):
     rich.pretty.pprint(cfg, expand_all=True)
 
+    # 1. 从配置里读取参数，转换为harl使用的格式
     algo_args = cfg.algorithm
     env_args = cfg.environment
 
@@ -17,7 +24,7 @@ def main(cfg: DictConfig):
     env_name = cfg.environment.name
     scenario_name = cfg.environment.scenario
 
-
+    # 2. 使用当前时间生成名称
     from datetime import datetime
     ts = datetime.now().strftime("%m%d-%H%M")
     run_name = f"[{algorithm_name}]<{scenario_name}>_{ts}"
@@ -31,26 +38,27 @@ def main(cfg: DictConfig):
     if env_name == 'pettingzoo_mw' and algo_args.train.get('episode_length') is not None:
         algo_args.train.episode_length = 500
     
-    def _to_dict(cfg1: DictConfig):
-        return omegaconf.OmegaConf.to_container(
-            cfg1, resolve=True, throw_on_missing=True
-        )
+    # 3. 初始化wandb
     wandb.init(project="HARL", 
                config=_to_dict(cfg), 
                sync_tensorboard=True, 
                name=run_name
                )
-    # start training
-    from harl.runners import RUNNER_REGISTRY
     
+    # 4. 整理参数，转换为dict以传导给harl
     algo_dict = _to_dict(algo_args)
     del algo_dict["name"]
+
     env_dict = _to_dict(env_args)
     del env_dict["name"]
     del env_dict["scenario"]
 
     runner = RUNNER_REGISTRY[algorithm_name](basic_info, algo_dict, env_dict)
+
+    # 5. 启动训练
     runner.run()
+
+    # 6. 训练完成
     runner.close()
     import requests
 
